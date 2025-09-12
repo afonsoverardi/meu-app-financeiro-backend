ec166f0306f450eb03902dab8b1c4557a9ba345d
afonsoverardi <88738677+afonsoverardi@users.noreply.github.com>
2025-09-12 01:47:22 -0300

    Update api.py

--- api.py
+++ api.py
@@ -95,7 +95,7 @@ def login():
     password = data.get('password')
     user = User.query.filter_by(email=email).first()
     if user and user.check_password(password):
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         return jsonify(access_token=access_token)
     return jsonify({"erro": "Credenciais inválidas"}), 401
 
