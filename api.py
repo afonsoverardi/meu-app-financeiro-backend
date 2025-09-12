7470ec9359b18baaaf030740c17e9e4a8a8248df
afonsoverardi <88738677+afonsoverardi@users.noreply.github.com>
2025-09-12 01:30:12 -0300

    Update api.py

--- api.py
+++ api.py
@@ -32,7 +32,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     # Relacionamentos (links para outras tabelas)
     compras = db.relationship('Compra', backref='user', lazy=True, cascade="all, delete-orphan")
